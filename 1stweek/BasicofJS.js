56f0f6b4de5b612970b4b7c8b23a070d107dc3e4
AnishGupta77 <anish.chandra01@gmail.com>
2026-04-20 09:37:51 +0530

    Add files via upload

---
+++ 1stweek/BasicofJS.js
@@ -0,0 +1,45 @@
+console.log(a)
+var a=10;
+
+/*console.log(b)
+let b=20;*/
+
+console.log(c)
+const c=30
+
+
+// helo*/
+
+/* hello
+dfdsf
+*/
+/*console.log("Hello Playwright");
+
+var phone=12345678
+console.log(phone);
+
+var interest=7.50
+console.log(interest);  
+
+var name=`hello`
+var name=`hola`
+console.log(name);
+
+var validaccountnumber = true
+console.log(typeof validaccountnumber);
+
+/* var xyx
+console.log(xyz); */
+
+/*var user=null
+console.log(user);
+
+let accountnum=1234
+accountnum=987
+console.log(accountnum);
+
+const accountNumber = 1234
+const accountNumber = 3455
+
+console.log(accountNumber);*/
+
---
+++ 1stweek/pracfirst.js
@@ -0,0 +1,90 @@
+/* function funnam()
+{
+    console.log("Hi I am function")
+}
+funnam()
+
+function add(a,b)
+{
+     
+    let c=a+b
+    return c
+
+}
+console.log (add(20,20))  */
+
+/*var a=10
+function scopeex()
+{
+    console.log(a);
+
+}
+console.log (scopeex(a));
+console.log(a);*/
+
+/*function demo()
+{
+    let xyx=10
+    console.log(xyx);
+}
+demo()
+console.log(xyx);*/
+
+/*function demo1()
+{
+    let a = 10;
+    const b = 20;
+    
+    console.log(a+b);
+}
+demo1()*/
+
+/*const browserVersion = "Chrome"
+function getBrowserVersion()
+{
+    if (browserVersion == "Chrome")
+    {
+        var browserVersion = "V6.1"
+        console.log("inside if cond", browserVersion);
+    }
+    console.log("outside if cond", browserVersion);
+
+}
+getBrowserVersion();*/
+
+//let a
+/*console.log(a)
+Greet()
+function Greet()
+{
+    console.log("GM");
+}
+var a=10;
+console.log(a);*/
+
+/*let a=50
+
+if (a>90)
+{
+    console.log("Grade A");
+}else if (a>60)
+{
+    console.log("Grade B");
+} else if(a<=50)
+{
+    console.log("Grade C");
+}*/
+
+let day= "Tuesday"
+
+switch(day){
+    case "Monday":
+        console.log("Working day");
+        break;
+    case "Sunday":
+        console.log("Sleeping day");
+        break;
+    default:
+        console.log("Normal day");
+        break;
+}
